cddbb515a98382d2e0300455ca4fc74c6a78cbb0
nilpatel <nilpatel62@gmail.com>
2024-12-05 09:42:28 +0530

    Added api for generate the chat using agent and get the agent chat response using response id.

--- app/db/mongodb.py
+++ app/db/mongodb.py
@@ -1,17 +1,22 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import settings
 
+
 class MongoDB:
     client: AsyncIOMotorClient = None
 
+
 db = MongoDB()
+
 
 async def get_database() -> AsyncIOMotorClient:
     return db.client
 
+
 async def connect_to_mongo():
     db.client = AsyncIOMotorClient(settings.mongodb_connection_string)
 
+
 async def close_mongo_connection():
     if db.client:
-        db.client.close() 
+        db.client.close()
