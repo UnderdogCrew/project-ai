9812c90f850be947e965e1c2bb09ae1b81d0946c
nilpatel <nilpatel62@gmail.com>
2024-12-27 10:44:42 +0530

    Refactor RAG response generation with OpenAI and Qdrant setup
    Introduce OpenAI integration, improve document search setup with `_setup_doc_search`, and replace outdated vector DB logic. Update retriever configuration to support similarity-based search, enhancing search precision and maintainability.

--- app/api/v1/endpoints/chat/generate_response.py
+++ app/api/v1/endpoints/chat/generate_response.py
@@ -36,6 +36,7 @@ from phi.tools.tavily import TavilyTools
 from phi.knowledge.website import WebsiteKnowledgeBase
 from phi.knowledge.pdf import PDFUrlKnowledgeBase
 from app.api.v1.endpoints.chat.db_helper import fetch_manage_data, save_ai_request
+from openai import OpenAI
 
 tools_list = {
     "send_email": EmailTools,
@@ -71,6 +72,16 @@ qdrant_api_key = os.getenv("QDRANT_API_KEY")
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ['OPENAI_API_TYPE'] = "openai"
+openai.api_type = "openai"
+open_ai_client = OpenAI()
+
+
+qdrant_client = QdrantClient(
+    url=qdrant_url,
+    api_key=qdrant_api_key,
+    timeout=300
+)
 
 
 def calculate_openai_cost(model: str, input_tokens: int, output_tokens: int, use_batch_api: bool = False,
@@ -257,6 +268,24 @@ def create_tool(tool_config):
         raise ValueError(f"Tool '{tool_name}' not found in tools_list")  # Raise an error if tool not found
 
 
+def _setup_doc_search(embedding_id):
+    """Sets up document search functionality"""
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small", allowed_special=(), disallowed_special=())
+    if not qdrant_client.collection_exists(embedding_id):
+        qdrant_client.create_collection(
+            collection_name=embedding_id,
+            vectors_config=VectorParams(size=1536, distance='Cosine')
+        )
+        print("Collection created in Qdrant.........!!!!!!!!!")
+
+    return QdrantVectorStore.from_existing_collection(
+            embedding=embeddings,
+            collection_name=embedding_id,
+            api_key=qdrant_api_key,
+            url=qdrant_url,
+        )
+
+
 def generate_rag_response(request: GenerateAgentChatSchema, response_id: str = None):
     try:
         message = request.message
@@ -330,24 +359,33 @@ def generate_rag_response(request: GenerateAgentChatSchema, response_id: str = N
 
         embedding_id = f"{str(rag_id)}"
 
+        # generate the response
+        user_embedding = open_ai_client.embeddings.create(
+            input=[message],
+            model="text-embedding-3-small"
+        )
+
+        if not user_embedding:
+            print("Error creating embeddings for context search.")
+
         # Initialize knowledge base if rag_id is available
         knowledge_base = None
         if rag_id:
-            vector_db = Qdrant(
-                collection=embedding_id,
-                url=qdrant_url,
-                api_key=qdrant_api_key,
-            )
-
-            # Perform a search in the vector database
-            search_data = vector_db.search(query=message, limit=1)
-            print(f"search_data: {search_data}")
-            urls = [search.name for search in search_data]
-            knowledge_base = WebsiteKnowledgeBase(
-                urls=urls,
-                vector_db=vector_db,
-            )
-        print(f"urls: {urls}")
+            # search_results = qdrant_client.search(
+            #     collection_name=embedding_id,
+            #     query_vector=user_embedding.data[0].embedding,
+            #     limit=5
+            # )
+            # if search_results:
+            #     for end_point in search_results:
+            #         print(end_point)
+            # urls = [search.name for search in search_data]
+            # knowledge_base = WebsiteKnowledgeBase(
+            #     urls=urls,
+            #     vector_db=vector_db,
+            # )
+            knowledge_base = _setup_doc_search(embedding_id=rag_id)
+        print(f"knowledge base: {knowledge_base}")
         # Format the system prompt based on the schema or message
         formatted_template = message
 
@@ -360,7 +398,11 @@ def generate_rag_response(request: GenerateAgentChatSchema, response_id: str = N
             tools=config_tools,
             # add_messages=sorted_data, # One of system, user, assistant, or tool.
             model=OpenAIChat(id=llm_config['model']),
-            knowledge=knowledge_base,
+            # knowledge=knowledge_base,
+            retriever=knowledge_base.as_retriever(
+                search_type="similarity_score_threshold",
+                search_kwargs={'score_threshold': 0.7, 'k': 5}
+            ),
             system_prompt=prompt,
             instructions=[additional_instructions],
             show_tool_calls=False,
